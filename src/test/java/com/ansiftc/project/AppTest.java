df88e91d24c01f6d04ce1418912692261a76e9d2
ansif2255 <ansiftc99@gmail.com>
2023-09-30 12:15:28 +0300

    add first commit

---
+++ src/test/java/com/ansiftc/project/AppTest.java
@@ -0,0 +1,20 @@
+package com.ansiftc.project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple_App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
